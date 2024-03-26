68731d593171ca1f2b1059c4e8adb07a7f617760
Fudgecake789 <omarbekhiet123@gmail.com>
2024-03-26 15:57:10 +0200

    Upgraded stage 2 and 3

--- Risk of getting Railed/Assets/Scripts/Moves/HeavySwing.cs
+++ Risk of getting Railed/Assets/Scripts/Moves/HeavySwing.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class HeavySwing : Move {
-    public HeavySwing() : base("Heavy Swing", "heavySwing", 5, 90, 0, 0) { }
+    public HeavySwing() : base("Heavy Swing", "heavySwing", 5, 25, 0, 0) { }
 
     public override bool Perform(Unit performer) {
         if (!base.Perform(performer)) return false;
