0bd99da26116b3e1b92cab0315c22c1d6fec6c32
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-03-31 04:58:52 +0200

    some updates to stage 2

---
+++ Risk of getting Railed/Assets/Scripts/Units/DragonMonster.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+public class DragonMonster : Unit {
+    private void Awake() {
+        YPos = -3.43f;
+        UnitName = "Dragon Warrior";
+        MaxHP = 1000;
+
+        // Moves = new Move[0];
+    }
+}
