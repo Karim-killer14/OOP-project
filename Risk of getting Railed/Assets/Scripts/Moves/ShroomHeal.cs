aa8d3f2c3bc97fb583bcae38dc0f90b1947bbebd
Fudgecake789 <omarbekhiet123@gmail.com>
2024-04-25 12:38:00 +0200

    Added sewer dwellers and will finish animations

---
+++ Risk of getting Railed/Assets/Scripts/Moves/ShroomHeal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ShroomHeal : Move
+{
+    private readonly int heal = 40;
+    public ShroomHeal() : base("ShroomHeal", 5) { }
+
+    public override bool Perform(Unit performer)
+    {
+        if (!base.Perform(performer)) return false;
+
+        performer.CurrentHP += heal;
+        return true;
+    }
+}
---
+++ Risk of getting Railed/Assets/Scripts/Moves/SkeleShield.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SkeleShield : Move
+{
+    public float shield = 4;
+    public SkeleShield() : base("SkeleShield", 4) { }
+
+    public override bool Perform(Unit performer)
+    {
+        if (!base.Perform(performer)) return false;
+
+        performer.CurrentSH += shield;
+
+        return true;
+    }
+}
