8199c150e68906e7902c786f4eb70d87fe60c27b
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-04-25 14:02:09 +0200

    fixed level order for bufs

--- Risk of getting Railed/Assets/Scripts/Moves/HeavySwing.cs
+++ Risk of getting Railed/Assets/Scripts/Moves/HeavySwing.cs
@@ -2,17 +2,15 @@ using UnityEngine;
 
 public class HeavySwing : Move {
     private readonly int damage = 90 * 9999;
-    private CameraShake camShake;
 
     public HeavySwing() : base("Heavy Swing", 5) { 
-        camShake = Camera.main.GetComponent<CameraShake>();
     }
 
     public override bool Perform(Unit performer) {
         if (!base.Perform(performer)) return false;
 
         performer.enemy.TakeDamage(damage);
-        camShake.Shake(0.075f, 0.7f, 0.15f);
+        Camera.main.GetComponent<CameraShake>().Shake(0.075f, 0.7f, 0.15f);
 
         return true;    
     }
