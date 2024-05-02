9aec3028c5f30f9cfa535bf76cd2aa313c8c68b7
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-05-02 13:33:27 +0300

    more heal for player, and made smaller man menu button

--- Risk of getting Railed/Assets/Scripts/Moves/Heal.cs
+++ Risk of getting Railed/Assets/Scripts/Moves/Heal.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class Heal : Move {
-    public float heal = 40;
+    public float heal = 50;
 
     public Heal(float dmgMult = 1) : base("Heal", 2) {
         heal *= dmgMult;
